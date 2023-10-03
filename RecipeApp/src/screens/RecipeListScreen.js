f8a6aeb0bc96177c07fc3f594c0b0dd52dcd26e2
otmp <47669979+Ryltc@users.noreply.github.com>
2023-10-03 17:54:01 -0400

    Update RecipeListScreen.js

--- RecipeApp/src/screens/RecipeListScreen.js
+++ RecipeApp/src/screens/RecipeListScreen.js
@@ -1,55 +1,67 @@
-import { StyleSheet, Text, View, SafeAreaView, ScrollView  } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, Animated  } from "react-native";
+import React, { useRef } from "react";
 import Header from "../components/Header";
 import SearchFilter from "../components/SearchFilter";
 import CategoriesFilter from "../components/CategoriesFilter";
 import SeasoningCard from "../components/SeasoningCard";
 import RecipeCard from "../components/RecipeCard";
 
+
+
 const RecipeListScreen = () => {
+    const scrollY = useRef(new Animated.Value(0)).current;
+
+    const translateHeader = scrollY.interpolate({
+        inputRange: [0, 80],
+        outputRange: [0, -80],
+        extrapolate: 'clamp',
+      });
+
     return (
-        <SafeAreaView style={{ backgroundColor: "#5e3c2c", flex: 1, paddingHorizontal: 20, marginTop: 50,  }}>
+      <SafeAreaView style={{ backgroundColor: "#5e3c2c", flex: 1, paddingHorizontal: 20, marginTop: 50 }}>
+        {/* render header */}
+        <Header headerText={`Hey Chef, lets cook!`} headerIcon="bell-o" />
 
-            {/* render header */}
-
-            <Header headerText={"Hi, ${username} "} headerIcon={"bell-o"}/>
-
-            {/* search filter */}
-
-            <SearchFilter icon="search" placeholder={"enter your fav recipe"}/>
-
-            {/* categories filter */}
-
+        <Animated.View style={{ transform: [{ translateY: translateHeader }] }}>
             <View>
-                <Text style={{ fontSize: 22, fontWeight: "300", }} > Catagories</Text>
+                {/* search filter */}
+                <SearchFilter icon="search" placeholder="enter your fav recipe" />
+                {/* categories filter */}
+                <View>
+                <Text style={{ fontSize: 22, fontWeight: "300" }}>Categories</Text>
 
                 {/* categories list */}
-
                 <CategoriesFilter />
-            </View>
-
-            <View>
+                </View>
+                <View>
                 <Text style={{ fontSize: 22, fontWeight: "300" }}>Seasoning</Text>
 
                 {/* seasoning list */}
-
                 <SeasoningCard />
+                </View>
             </View>
+        </Animated.View>
+
+        <Animated.ScrollView
+            contentContainerStyle={styles.content}
+            onScroll={Animated.event([{ nativeEvent: { contentOffset: { y: scrollY } } }], {
+            useNativeDriver: true,
+            })}
+            scrollEventThrottle={1}
+        >
 
             {/* recipe list filter */}
+            <View style={{ marginTop: 5, flex: 1 }}>
+              <Text style={{ fontSize: 22, fontWeight: "300" }}>Recipes</Text>
 
-            <View style={{ marginTop: 5, flex: 1}}>
-                <Text style={{ fontSize: 22, fontWeight: "300" }}>Recipes</Text>
-
-                {/* recipe list */}
-
-                <RecipeCard />
-
+              {/* recipe list */}
+              <RecipeCard />
             </View>
+        </Animated.ScrollView>
         </SafeAreaView>
-    );
-};
+        );
+      };
 
-export default RecipeListScreen;
+      export default RecipeListScreen;
 
-const styles = StyleSheet.create({});
+      const styles = StyleSheet.create({});
